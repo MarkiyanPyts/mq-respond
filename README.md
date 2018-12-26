14c5a2dbf9c7404f139c6f3362f261a50c087747
Markiyan Pyts <markiyan.pyts@osf-commerce.com>
2018-12-26 22:31:51 +0200

    added readme and library

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # mq-respond
 Vanilla js lib which handles responsive javascript using matchMedia method
+
+## Installation
+Include the mqRes in your page via script tag or require it from npm.
+
+There is 2 mqRes.js and mqRes.js files in repo.
+
+## Usage
+Lib creates class mqRes on window object, you need to create an instance of it and pass array with media queries objects to it.
+
+```
+var mqRespond = new mqRes([
+    {
+        label: 'phone',
+        from: 0,
+        to: 479
+    },
+    {
+        label: 'phoneLandscape',
+        from: 480,
+        to: 767
+    },
+    {
+        label: 'tablet',
+        from: 768,
+        to: 1023
+    },
+    {
+        label: 'desktop',
+        from: 1024,
+        to: 1679
+    },
+    {
+        label: 'desktopLarge',
+        from: 1680
+    }
+]);
+
+mqRespond.add(function (status) {
+    console.log('desktopL:', status.desktopLarge);
+});
+
+mqRespond.add(function (status) {
+    console.log('desktopL2:', status.desktopLarge);
+});
+```
+
+Each object has the following properties:   
+label - label of the beakpoint, needs to be one word since it will be used in js later   
+from - from where breakpoint starts   
+to - where breakpoint ends   
+
+if `to` in not specified code will create only `min-width` media query
+
+Now when you have initialized mqRes instance you can add multiple callbacks to it via `add` method.
+
+`add` method accepts callback parameter which provides `status` parameter which will provide info per media config array you used as constructor parameter.
+
+e.g:   
+```
+{
+    desktop: false
+    desktopLarge: true
+    phone: false
+    phoneLandscape: false
+    tablet: false
+}
+```
+
+So in your callbacks you can write code like:
+```
+mqRespond.add(function (status) {
+    if (status.phone || status.phoneLandscape || status.tablet) {
+        console.log('Do something you need on mobile only');
+    }
+
+    if (status.desktop || status.desktopLarge) {
+        console.log('Do something you need on desktop only');
+    }
+
+    if (!status.phone) {
+        console.log('Do something which you want to do everywhere but mobile viewport');
+    }
+});
+```
+
---
+++ mqRes.js
@@ -0,0 +1,104 @@
+window.mqRes = function (mediaQueries) {
+    this.status = {};
+    this.watchers = [];
+    this.matchMedia = [];
+    this.mediaQueries = mediaQueries || null;
+    this.labels = [];
+    this.callbacks = [];
+    this.isMediaAttached = false;
+
+    this.init(mediaQueries);
+};
+
+window.mqRes.prototype = {
+    init: function (mediaQueries) {
+        this.mediaQueries = mediaQueries;
+
+        if (!this.mediaQueries) {
+            throw 'mqRes first argument should be array with media queries object!!!!';
+        }
+
+        this.attachMedia(this.mediaQueries);
+    },
+    attachMedia: function (mediaQueries) {
+        var that = this;
+
+        if (that.isMediaAttached) {
+            that.watchers.forEach(function (item) {
+                item.removeListener(that.responseMedia.bind(that));
+            });
+        }
+
+        mediaQueries.forEach(function (item) {
+            var mediaQuery = '',
+                index = 0;
+
+            if (item.from !== undefined) {
+                mediaQuery += '(min-width: ' + item.from + 'px)';
+            }
+
+            if (item.to !== undefined) {
+                if (item.from !== undefined) {
+                    mediaQuery += ' and (max-width: ' + item.to + 'px)';
+                } else {
+                    mediaQuery += '(max-width: ' + item.to + 'px)';
+                }
+            }
+
+            that.watchers.push(window.matchMedia(mediaQuery));
+            index = that.watchers.length - 1;
+
+            that.labels.push({
+                label: item.label,
+                media: that.watchers[index].media
+            });
+
+            that.responseMedia(that.watchers[index]);
+            that.watchers[index].addListener(that.responseMedia.bind(that));
+        });
+
+        this.isMediaAttached = true;
+    },
+    responseMedia: function (watcher) {
+        var that = this,
+            matchingLabel = that.getMatchingLabel(watcher);
+
+        if (watcher.matches) {
+            if (matchingLabel) {
+                that.status[matchingLabel] = true;
+            }
+        } else if (matchingLabel) {
+            that.status[matchingLabel] = false;
+        }
+
+        if (watcher.matches) {
+            setTimeout(function () {
+                that.callbacks.forEach(function (callback) {
+                    callback(that.status);
+                });
+            }, 10);
+        }
+    },
+    getMatchingLabel: function (watcher) {
+        var matchingLabel = false;
+
+        this.labels.forEach(function (item) {
+            if (item.media === watcher.media) {
+                matchingLabel = item.label;
+            }
+        });
+
+        return matchingLabel;
+    },
+    add: function (callback) {
+        if (typeof callback === 'function') {
+            if (!this.mediaQueries.length) {
+                throw 'media queries list is not set, please set it and try again';
+            }
+
+            this.callbacks.push(callback);
+        } else {
+            throw 'callback must be a function';
+        }
+    }
+};
